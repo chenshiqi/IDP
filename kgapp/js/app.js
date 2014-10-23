b70878490524d6dc439e23cdcb861f89a1dfd150
Shiqi Chen <shiqi.chen.2012@sis.smu.edu.sg>
2014-10-23 19:16:11 +0800

    Started on uploadImage on add_item

--- kgapp/js/app.js
+++ kgapp/js/app.js
@@ -1,15 +1,15 @@
-var myApplication = angular.module('myApplication', ['ngRoute', 'angularModalService']);
+var myApplication = angular.module('myApplication', ['ngRoute', 'angularModalService', 'angularFileUpload']);
 
 myApplication.config(function ($routeProvider) {
-    $routeProvider.when('/welcome', {templateUrl: 'partials/welcome.html', controller:''});
+    $routeProvider.when('/welcome', {templateUrl: 'partials/welcome.html', controller: ''});
     $routeProvider.when('/all_listings', {templateUrl: 'partials/all_listings.html', controller: 'ItemListCtrl'});
     $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
     $routeProvider.when('/register', {templateUrl: 'partials/register.html', controller: ''});
     $routeProvider.when('/register/seller', {templateUrl: 'partials/register_seller.html', controller: ''});
     $routeProvider.when('/register/buyer', {templateUrl: 'partials/register_buyer.html', controller: ''});
-    $routeProvider.when('/add-item', {templateUrl: 'partials/add_item.html', controller:''}); //to add controller for addItem
-    $routeProvider.when('/edit-item', {templateUrl: 'partials/edit_item.html', controller:'EditItemCtrl'}); //route according to item ID
-    $routeProvider.when('/faq', {templateUrl: 'partials/faq.html', controller:''});
+    $routeProvider.when('/add-item', {templateUrl: 'partials/add_item.html', controller: ''}); //to add controller for addItem
+    $routeProvider.when('/edit-item', {templateUrl: 'partials/edit_item.html', controller: 'EditItemCtrl'}); //route according to item ID
+    $routeProvider.when('/faq', {templateUrl: 'partials/faq.html', controller: ''});
     $routeProvider.when('/seller/profile/monicacheng', {templateUrl: 'partials/seller_profile.html'});
     $routeProvider.when('/seller/profile/monicacheng/send-offer', {templateUrl: 'partials/send_offer.html'});
     $routeProvider.when('/buyer/profile/onglyetat', {templateUrl: 'partials/buyer_profile.html'});
@@ -17,7 +17,7 @@ myApplication.config(function ($routeProvider) {
     $routeProvider.when('/buyer/onglyetat/myoffers', {templateUrl: 'partials/offer_buyer.html'});
     $routeProvider.when('/leave-feedback', {templateUrl: 'partials/feedback.html'});
     $routeProvider.when('/send-offer', {templateUrl: 'partials/send_offer.html'});
-    $routeProvider.otherwise({redirectTo:'/welcome'});
+    $routeProvider.otherwise({redirectTo: '/welcome'});
 });
 
 myApplication.factory('dataFactory', function () {
@@ -50,56 +50,56 @@ myApplication.controller('ItemListCtrl', ['$scope', '$http',
         });
     }]);
 
- myApplication.controller('MainController', function($scope, $route, $routeParams, $location) {
-     $scope.$route = $route;
-     $scope.$location = $location;
-     $scope.$routeParams = $routeParams;
- });
- 
+myApplication.controller('MainController', function ($scope, $route, $routeParams, $location) {
+    $scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+});
+
 myApplication.controller('LoginCtrl', ['$scope', '$http',
     function ($scope, $http) {
-    $http.get('json/users.json').success(function(data){
-        $scope.users = data;   
-            for(var i = 0; i < data.length; i++){
-                if(data[i].email == email && data[i].password == password){
-                    window.location.href= "/kgapp/index_seller.html";
+        $http.get('json/users.json').success(function (data) {
+            $scope.users = data;
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].email == email && data[i].password == password) {
+                    window.location.href = "/kgapp/index_seller.html";
                 }
             }
-    });
-    
-}]);
-
-myApplication.controller('EditItemCtrl', ['$scope', '$http', 
-    function($scope, $http) {
-        $http.get('json/Item.json').success(function (data){
-           $scope.items = data; 
         });
-    
-}]);
 
-myApplication.controller('SellerUserCtrl', ['$scope', '$http', 
-    function($scope, $http) {
-        $http.get('json/Seller.json').success(function (data){
-           $scope.seller = data; 
-        });
-    
-}]);
+    }]);
 
-myApplication.controller('BuyerUserCtrl', ['$scope', '$http', 
-    function($scope, $http) {
-        $http.get('json/Buyer.json').success(function (data){
-           $scope.buyer = data; 
+myApplication.controller('EditItemCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+        $http.get('json/Item.json').success(function (data) {
+            $scope.items = data;
         });
-    
-}]);
 
-myApplication.controller('BuyerDealsCtrl', ['$scope', '$http', 
-    function($scope, $http) {
-        $http.get('json/BuyerDeals.json').success(function (data){
-           $scope.items = data; 
+    }]);
+
+myApplication.controller('SellerUserCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+        $http.get('json/Seller.json').success(function (data) {
+            $scope.seller = data;
         });
-    
-}]);
+
+    }]);
+
+myApplication.controller('BuyerUserCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+        $http.get('json/Buyer.json').success(function (data) {
+            $scope.buyer = data;
+        });
+
+    }]);
+
+myApplication.controller('BuyerDealsCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+        $http.get('json/BuyerDeals.json').success(function (data) {
+            $scope.items = data;
+        });
+
+    }]);
 
 //to assign logic of displaying correct nav bar based on user log in
 $(function () {
@@ -114,44 +114,97 @@ $(function () {
     $("#includedNavBar").load("./templates/nav.html");
 });
 
-myApplication.controller('ModalServiceCtrl', function($scope, ModalService) {
-    
-    $scope.show = function() {
+myApplication.controller('ModalServiceCtrl', function ($scope, ModalService) {
+
+    $scope.show = function () {
         ModalService.showModal({
             templateUrl: 'modal.html',
             controller: "ModalCloseCtrl"
-        }).then(function(modal) {
+        }).then(function (modal) {
             modal.element.modal();
-            modal.close.then(function(result) {
+            modal.close.then(function (result) {
                 $scope.message = "You said " + result;
             });
         });
     };
-    
+
 });
 
-myApplication.controller('ModalServiceFeedbackCtrl', function($scope, ModalService) {
-    
-    $scope.show = function() {
+myApplication.controller('ModalServiceFeedbackCtrl', function ($scope, ModalService) {
+
+    $scope.show = function () {
         ModalService.showModal({
             templateUrl: 'feedback.html',
             controller: "ModalCloseCtrl"
-        }).then(function(modal) {
+        }).then(function (modal) {
             modal.element.modal();
-            modal.close.then(function(result) {
+            modal.close.then(function (result) {
                 $scope.message = "You said " + result;
             });
         });
     };
-    
-});
-
-myApplication.controller('ModalCloseCtrl', function($scope, close) {
-  
- $scope.close = function(result) {
- 	close(result, 500); // close, but give 500ms for bootstrap to animate
- };
-
 
 });
-    
+
+myApplication.controller('ModalCloseCtrl', function ($scope, close) {
+
+    $scope.close = function (result) {
+        close(result, 500); // close, but give 500ms for bootstrap to animate
+    };
+
+
+});
+
+myApplication.controller('UploadController', ['$scope', 'FileUploader', function ($scope, FileUploader) {
+        var uploader = $scope.uploader = new FileUploader({
+            url: 'upload.json'
+        });
+
+        // FILTERS
+
+        uploader.filters.push({
+            name: 'imageFilter',
+            fn: function (item /*{File|FileLikeObject}*/, options) {
+                var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+                return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+            }
+        });
+
+        // CALLBACKS
+
+        uploader.onWhenAddingFileFailed = function (item /*{File|FileLikeObject}*/, filter, options) {
+            console.info('onWhenAddingFileFailed', item, filter, options);
+        };
+        uploader.onAfterAddingFile = function (fileItem) {
+            console.info('onAfterAddingFile', fileItem);
+        };
+        uploader.onAfterAddingAll = function (addedFileItems) {
+            console.info('onAfterAddingAll', addedFileItems);
+        };
+        uploader.onBeforeUploadItem = function (item) {
+            console.info('onBeforeUploadItem', item);
+        };
+        uploader.onProgressItem = function (fileItem, progress) {
+            console.info('onProgressItem', fileItem, progress);
+        };
+        uploader.onProgressAll = function (progress) {
+            console.info('onProgressAll', progress);
+        };
+        uploader.onSuccessItem = function (fileItem, response, status, headers) {
+            console.info('onSuccessItem', fileItem, response, status, headers);
+        };
+        uploader.onErrorItem = function (fileItem, response, status, headers) {
+            console.info('onErrorItem', fileItem, response, status, headers);
+        };
+        uploader.onCancelItem = function (fileItem, response, status, headers) {
+            console.info('onCancelItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteItem = function (fileItem, response, status, headers) {
+            console.info('onCompleteItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteAll = function () {
+            console.info('onCompleteAll');
+        };
+
+        console.info('uploader', uploader);
+    }]);

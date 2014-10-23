9dd5f8c05170ea4b531c185f59c77e5ff25d5ec6
Shiqi Chen <shiqi.chen.2012@sis.smu.edu.sg>
2014-10-23 19:45:54 +0800

    Added upload images function in add_item.html

--- kgapp/js/app.js
+++ kgapp/js/app.js
@@ -50,36 +50,36 @@ myApplication.controller('ItemListCtrl', ['$scope', '$http',
             $scope.items = data;
         });
     }]);
- myApplication.controller('MainController', function($scope, $route, $routeParams, $location) {
-     $scope.$route = $route;
-     $scope.$location = $location;
-     $scope.$routeParams = $routeParams;
- });
- 
-myApplication.controller('LoginCtrl', function ($scope, $http) {
-
-    $http.get('json/users.json').success(function(data){
-        $scope.users = data;   
-        
-            $scope.login = function(){
-                for(var i = 0; i < data.length; i++){
-                if(data[i].email == $scope.email && data[i].password == $scope.password){
-                    
-                    $scope.user = $scope.email;
-                    console.log("what");
-                    window.location.href= "/kgapp/index_" + data[i].type+ ".html";
-                }
-            }
-            }
-            
-    });
-    
+myApplication.controller('MainController', function ($scope, $route, $routeParams, $location) {
+    $scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
 });
 
-myApplication.controller('EditItemCtrl', ['$scope', '$http', 
-    function($scope, $http) {
-        $http.get('json/Item.json').success(function (data){
-           $scope.items = data; 
+myApplication.controller('LoginCtrl', function ($scope, $http) {
+
+    $http.get('json/users.json').success(function (data) {
+        $scope.users = data;
+
+        $scope.login = function () {
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].email == $scope.email && data[i].password == $scope.password) {
+
+                    $scope.user = $scope.email;
+                    console.log("what");
+                    window.location.href = "/kgapp/index_" + data[i].type + ".html";
+                }
+            }
+        }
+
+    });
+
+});
+
+myApplication.controller('EditItemCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+        $http.get('json/Item.json').success(function (data) {
+            $scope.items = data;
         });
 
     }]);
@@ -172,7 +172,7 @@ myApplication.controller('ModalCloseCtrl', function ($scope, close) {
 
 myApplication.controller('UploadController', ['$scope', 'FileUploader', function ($scope, FileUploader) {
         var uploader = $scope.uploader = new FileUploader({
-            url: 'upload.json'
+            url: '/json/upload.json'
         });
 
         // FILTERS
@@ -223,3 +223,49 @@ myApplication.controller('UploadController', ['$scope', 'FileUploader', function
 
         console.info('uploader', uploader);
     }]);
+
+myApplication.directive('ngThumb', ['$window', function($window){
+        var helper = {
+            support: !!($window.FileReader && $window.CanvasRenderingContext2D),
+            isFile: function(item) {
+                return angular.isObject(item) && item instanceof $window.File;
+            },
+            isImage: function(file) {
+                var type =  '|' + file.type.slice(file.type.lastIndexOf('/') + 1) + '|';
+                return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+            }
+        };
+
+        return {
+            restrict: 'A',
+            template: '<canvas/>',
+            link: function(scope, element, attributes) {
+                if (!helper.support) return;
+
+                var params = scope.$eval(attributes.ngThumb);
+
+                if (!helper.isFile(params.file)) return;
+                if (!helper.isImage(params.file)) return;
+
+                var canvas = element.find('canvas');
+                var reader = new FileReader();
+
+                reader.onload = onLoadFile;
+                reader.readAsDataURL(params.file);
+
+                function onLoadFile(event) {
+                    var img = new Image();
+                    img.onload = onLoadImage;
+                    img.src = event.target.result;
+                }
+
+                function onLoadImage() {
+                    var width = params.width || this.width / this.height * params.height;
+                    var height = params.height || this.height / this.width * params.width;
+                    canvas.attr({ width: width, height: height });
+                    canvas[0].getContext('2d').drawImage(this, 0, 0, width, height);
+                }
+            }
+        };       
+        
+}]);

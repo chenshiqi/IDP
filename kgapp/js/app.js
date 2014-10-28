64a9cbd52662ad4c6c236c8334079294b22bc887
Shiqi Chen <shiqi.chen.2012@sis.smu.edu.sg>
2014-10-28 11:44:31 +0800

    hardcoded version protoype

--- kgapp/js/app.js
+++ kgapp/js/app.js
@@ -3,6 +3,7 @@ var myApplication = angular.module('myApplication', ['ngRoute', 'angularModalSer
 myApplication.config(function ($routeProvider) {
     $routeProvider.when('/welcome', {templateUrl: 'partials/welcome.html', controller: ''});
     $routeProvider.when('/all_listings', {templateUrl: 'partials/all_listings.html', controller: 'ItemListCtrl'});
+    $routeProvider.when('/all_listings2', {templateUrl: 'partials/all_listings_NoSpeaker.html', controller: 'ItemListCtrlNoSpeaker'});
     $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
     $routeProvider.when('/register', {templateUrl: 'partials/register.html', controller: ''});
     $routeProvider.when('/register/seller', {templateUrl: 'partials/register_seller.html', controller: ''});
@@ -12,7 +13,9 @@ myApplication.config(function ($routeProvider) {
     $routeProvider.when('/faq', {templateUrl: 'partials/faq.html', controller: ''});
     $routeProvider.when('/seller/profile/monicacheng', {templateUrl: 'partials/seller_profile.html'});
     $routeProvider.when('/seller/profile/monicacheng1', {templateUrl: 'partials/seller_profile_Pending.html'});
+    $routeProvider.when('/seller/profile/monicacheng2', {templateUrl: 'partials/seller_profile_sold.html'});
     $routeProvider.when('/seller/profile/monicacheng/send-offer', {templateUrl: 'partials/send_offer.html'});
+    $routeProvider.when('/buyer/viewProfile/monicacheng', {templateUrl: 'partials/seller_profile_view.html', controller: 'MonicaItemListCtrl'});
     $routeProvider.when('/buyer/profile/onglyetat', {templateUrl: 'partials/buyer_profile.html'});
     $routeProvider.when('/seller/monicacheng/myoffers', {templateUrl: 'partials/offer_seller.html'});
     $routeProvider.when('/buyer/onglyetat/myoffers', {templateUrl: 'partials/offer_buyer.html'});
@@ -20,8 +23,8 @@ myApplication.config(function ($routeProvider) {
     $routeProvider.when('/send-offer', {templateUrl: 'partials/send_offer.html'});
 
 
-    $routeProvider.when('/index_seller', {templateUrl:'index_seller.html', controller:'LoginCtrl'});
-    $routeProvider.when('/index_buyer', {templateUrl:'index_buyer.html', controller:'LoginCtrl'});
+    //$routeProvider.when('/index_seller', {templateUrl:'index_seller.html', controller:'LoginCtrl'});
+    //$routeProvider.when('/index_buyer', {templateUrl:'index_buyer.html', controller:'LoginCtrl'});
     $routeProvider.otherwise({redirectTo:'/welcome'});
 
 });
@@ -55,25 +58,32 @@ myApplication.controller('ItemListCtrl', ['$scope', '$http',
             $scope.items = data;
         });
     }]);
+
+myApplication.controller('ItemListCtrlNoSpeaker', ['$scope', '$http',
+    function ($scope, $http) {
+        $http.get('json/Item_NoSpeaker.json').success(function (data) {
+            $scope.itemsNoSpeaker = data;
+        });
+    }]);
+
+myApplication.controller('MonicaItemListCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+        $http.get('json/monica_Items.json').success(function (data) {
+            $scope.items = data;
+        });
+    }]);
+
+
 myApplication.controller('MainController', function ($scope, $route, $routeParams, $location) {
     $scope.$route = $route;
     $scope.$location = $location;
     $scope.$routeParams = $routeParams;
 });
- myApplication.controller('MainController', function($scope, $route, $routeParams, $location) {
-     $scope.$route = $route;
-     $scope.$location = $location;
-     $scope.$routeParams = $routeParams;
-     $("#includedNavBar").load("./templates/nav.html");
-     
- });
 
 
-
- 
 myApplication.controller('LoginCtrl', function ($scope, $http,$rootScope, $location) {
 
-    $http.get('json/users.json').success(function(data){
+   /** $http.get('json/users.json').success(function(data){
         $scope.users = data;   
         var name;
         var pos;
@@ -119,17 +129,10 @@ myApplication.controller('LoginCtrl', function ($scope, $http,$rootScope, $locat
         console.log(pos);
         console.log(name);
             
-    });
+    }); **/
 
 });
 
-myApplication.controller('EditItemCtrl', ['$scope', '$http',
-    function ($scope, $http) {
-        $http.get('json/Item.json').success(function (data) {
-            $scope.items = data;
-        });
-
-    }]);
 
 myApplication.controller('EditItemCtrl', ['$scope', '$http',
     function ($scope, $http) {
@@ -165,17 +168,17 @@ myApplication.controller('BuyerDealsCtrl', ['$scope', '$http',
 
 //to assign logic of displaying correct nav bar based on user log in
 
-//$(function () {
-//    $("#includedBuyerNavBar").load("./templates/navBuyer.html");
-//});
-//
-//$(function () {
-//    $("#includedSellerNavBar").load("./templates/navSeller.html");
-//});
-//
-//$(function () {
-//    $("#includedNavBar").load("./templates/nav.html");
-//});
+$(function () {
+    $("#includedBuyerNavBar").load("./templates/navBuyer.html");
+});
+
+$(function () {
+    $("#includedSellerNavBar").load("./templates/navSeller.html");
+});
+
+$(function () {
+    $("#includedNavBar").load("./templates/nav.html");
+});
 
 
 myApplication.controller('ModalServiceCtrl', function ($scope, ModalService) {
